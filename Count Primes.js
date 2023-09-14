ada1965f6cc434704c658af9f3293426d5625173
Maria Arkhipova <marias1403@yandex.ru>
2023-09-14 12:02:47 +0900

    feat: count primes added

---
+++ Count Primes.js
@@ -0,0 +1,19 @@
+//Решето Эрастофена
+var countPrimes = function(n) {
+  if (n <= 2) {
+    return 0;
+  }
+
+  const isPrime = new Array(n).fill(true);
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  return isPrime.filter((value) => value).length;
+};
